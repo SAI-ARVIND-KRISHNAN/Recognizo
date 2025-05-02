5ac59403d0cf4a876043a697f60fa6ec9cbb6fa8
SaiArvind576 <skris179@asu.edu>
2025-05-01 17:59:13 -0700

    webcam feature implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WebcamComponent } from './webcam/webcam.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WebcamComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WebcamComponent } from './webcam/webcam.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WebcamComponent
+  }
+];
